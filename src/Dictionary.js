9f914246372e61e04549daa3d5910388d9bcff14
ssvetluna <s.stepanenko.a@gmail.com>
2022-10-05 16:14:42 +0300

    Added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    return "";
+  }
+
   function Search(event) {
     event.preventDefault();
     alert(`Searching ${keyword}`);
