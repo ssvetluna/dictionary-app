231a51e1a27724b4ac07a5aeeabec03989920c5f
ssvetluna <s.stepanenko.a@gmail.com>
2022-10-05 16:19:55 +0300

    Changes in api call

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,7 +14,7 @@ export default function Dictionary() {
 
   function Search(event) {
     event.preventDefault();
-    alert(`Searching ${keyword}`);
+    axios.get(url).then(handleResponse);
   }
   function handleKeyword(event) {
     setKeyword(event.target.value);
